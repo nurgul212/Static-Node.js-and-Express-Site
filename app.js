c8fd36214d9b9f1fc2d752a5c11ec4ffc4ca4500
Nurgul Amat <nurgulamat212@gmail.com>
2022-02-08 22:05:47 -0600

    set the home page

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const express = require('express');
+const {projects} = require('./data.json');
+
 const app = express();
+
+//Use a static route and the express.static method to serve the static files located in the public folder, like images, css, js
+app.use('/static', express.static('public'));
+
+// set view engine to pug
+app.set('view engine', 'pug');
+
+// app.get()
+app.get('/', (req, res) => {
+    res.render('index', {projects})
+});
+
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>{
+    console.log(`The server is running on local host: ${port}!`);
+});
